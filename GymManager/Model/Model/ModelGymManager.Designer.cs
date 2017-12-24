96eb5cd3941ca82635bccd3c16ae917ee4a619b7
Fede <fedeverdini@gmail.com>
2017-12-24 15:26:22 -0300

    Actualización modelo
    Actualización modelo

--- GymManager/Model/Model/ModelGymManager.Designer.cs
+++ GymManager/Model/Model/ModelGymManager.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\soluciones\Documents\Visual Studio 2017\Projects\GymManager\GymManager\Project1\Model\ModelGymManager.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\soluciones\Documents\Visual Studio 2017\Projects\GymManager\GymManager\Model\Model\ModelGymManager.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
